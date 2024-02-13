affa1100a8d89cbc9092f9f9da626a80637b9d91
Lavanya <1999lavanyakona@gmail.com>
2024-02-13 15:38:36 -0600

    Servlets - Basic Login and Logout Controllers

---
+++ src/main/java/servlets/LogoutServlet.java
@@ -0,0 +1,15 @@
+package servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        response.sendRedirect("login.html");
+    }
+}
